488e4b312d9c1147644c8e419f9570d49c075220
shreya <shreya.r2022cse@sece.ac.in>
2025-08-20 11:38:01 +0530

    Fix CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -11,27 +11,16 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-const allowedOrigins = [
+app.use(cors({
+    origin: [
         "http://localhost:3000",
         "https://ecommerce-sooty-rho.vercel.app/",
         "https://admin-ecommerce-chi.vercel.app/"
-
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps, curl, postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = "CORS policy: This origin is not allowed.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+    credentials: true
+}));
+
 // Ensure upload directory exists
 const uploadDir = './upload/images';
 if (!fs.existsSync(uploadDir)) {
