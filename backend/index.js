d87528cea14098b878f0c239f374c555aad1ea1d
shreya <shreya.r2022cse@sece.ac.in>
2025-08-20 11:45:50 +0530

    Fix CORS config and Google Fonts link

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://ecommerce-sooty-rho.vercel.app/",
+        "https://e-commerce-theta-vert.vercel.app/",
         "https://admin-ecommerce-chi.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
